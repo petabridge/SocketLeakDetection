a357948289b92b3ee8a0090a6ea54d9ea2b38ce0
Aaron Stannard <aaron@petabridge.com>
2019-04-16 14:54:12 -0500

    removed unused classes (#20)

--- src/SocketLeakDetection/TcpPortUseSupervisor.cs
+++ src/SocketLeakDetection/TcpPortUseSupervisor.cs
@@ -58,86 +58,11 @@ namespace SocketLeakDetection
             });
         }
 
-        private long MaxConnections { get; set; }
-        private double PercentDifference { get; set; }
-        private double MaxDifference { get; set; }
-        private int LargeSample { get; set; }
-        private int SmallSample { get; set; }
-
         protected override void PreStart()
         {
             _tcpScanner = Context.ActorOf(Props.Create(() => new TcpPortMonitoringActor(Self, _settings)),
                 "portMonitor");
             Context.Watch(_tcpScanner);
-        }
-
-        /// <summary>
-        ///     Sets configuration variables for Percent Difference actor.
-        /// </summary>
-        /// <param name="config"></param>
-        private void GetConfig(Config config)
-        {
-            var actorConfig = config.GetConfig("SLD");
-            if (actorConfig != null)
-            {
-                var mx = Convert.ToInt64(actorConfig.GetString("Max-Connections",
-                    "16777214")); // Set value to theoretical max TCP connections for Windows Server 2003 https://docs.microsoft.com/en-us/previous-versions/windows/it-pro/windows-server-2003/cc758980(v=ws.10)
-                if (mx > 0) // If no value is given.
-                {
-                    MaxConnections = mx;
-                }
-                else
-                {
-                    _log.Warning("Value for Max Number of Connection not given, setting to 16777214");
-                    MaxConnections = 16777214;
-                }
-
-                var pd = Convert.ToDouble(actorConfig.GetString("Percent-Difference", "0.20"));
-                if (pd < 1 && pd > 0)
-                {
-                    PercentDifference = pd;
-                }
-                else
-                {
-                    _log.Warning("Percent Difference value not between 0 and 1, setting to 0.20");
-                    PercentDifference = 0.20;
-                }
-
-                var md = Convert.ToDouble(actorConfig.GetString("Max-Difference", "0.25"));
-                if (md < 1 && md > 0)
-                {
-                    MaxDifference = md;
-                }
-                else
-                {
-                    _log.Warning("Max Difference value not between 0 and 1, setting to 0.25");
-                    MaxDifference = 0.25;
-                }
-
-                var ls = Convert.ToInt32(actorConfig.GetString("Large-Sample", "120"));
-                if (ls > 2)
-                {
-                    LargeSample = ls;
-                }
-                else
-                {
-                    _log.Warning(
-                        "Large Sample must be greater than 2, setting to 120"); //120 sets a sample size of 1 minute when readings are done every 500 milliseconds
-                    LargeSample = 120;
-                }
-
-                var ss = Convert.ToInt32(actorConfig.GetString("Small-Sample",
-                    "20")); //20 sets a sample size of 10 seconds when readings are done every 500 milliseconds
-                if (ss < ls)
-                {
-                    SmallSample = ss;
-                }
-                else
-                {
-                    _log.Warning("Small Sample must be greater than 1 and smaller than Large Sample, setting to 1");
-                    SmallSample = 1; //alpha cannot be bigger than 1: Alpa = 2/(samplesize +1)
-                }
-            }
         }
 
         /// <summary>
